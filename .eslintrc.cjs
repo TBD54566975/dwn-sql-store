dd7a95f29283d69aeceaf601c59939d5cc790dab
Adam Mika <amika@squareup.com>
2023-07-18 11:21:16 -0600

    Update config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  root          : true,
   extends       : ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser        : '@typescript-eslint/parser',
   parserOptions : {
