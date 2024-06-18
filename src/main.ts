e0c996c3ebb86c50b21be5135f37d29595d2a21d
Henry Tsai <henrytsai@outlook.com>
2024-06-18 09:14:45 -0700

    Missing export + v0.5.1 (#40)

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,4 @@ export * from './dialect/sqlite-dialect.js';
 export * from './data-store-sql.js';
 export * from './event-log-sql.js';
 export * from './message-store-sql.js';
+export * from './resumable-task-store-sql.js';
